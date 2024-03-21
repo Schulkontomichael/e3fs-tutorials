8ca6577ad9459bc6f9f12ba4c188339348e41181
Schulkontomichael <152477766+Schulkontomichael@users.noreply.github.com>
2024-03-21 07:24:30 +0000

    Pending changes exported from your codespace

--- tsdev/src/index.ts
+++ tsdev/src/index.ts
@@ -11,6 +11,7 @@
 
 import express, { Request, Response } from 'express';
 import { Queue } from './Queue/queue';
+import { register } from 'module';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,49 +19,71 @@ const port = process.env.PORT || 3000;
 
 var bodyParser = require('body-parser')
 
-app.post('/', (req: Request, res: Response) => {
-    const queue: Queue<any> = {
-        elements: [],
-        capacity: 0,
-        length: function (): number {
-            throw new Error('Function not implemented.');
-        },
-        setCapacity: function (capacity: number): void {
-            throw new Error('Function not implemented.');
-        },
-        getCapacity: function (): number {
-            throw new Error('Function not implemented.');
-        },
-        enqueue: function (item: any): void {
-            throw new Error('Function not implemented.');
-        },
-        dequeue: function () {
-            throw new Error('Function not implemented.');
-        },
-        peek: function () {
-            throw new Error('Function not implemented.');
-        }
-    }
-    queue.push(queue);
-    res.status(201).json(queue);
+// https://expressjs.com/en/resources/middleware/body-parser.html
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.set('view engine', 'ejs');
+
+let queue: Queue<any> = new Queue<any>();
+
+app.post('/queue/setcapacity', (req: Request, res: Response) => { 
+    queue.setCapacity(req.body.value);
+    res.send('Die Kapazität wurde auf ' + queue.getCapacity() + ' gesetzt.');
 });
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, TypeScript Express!');
+app.get('/about', function(req, res) {
+    res.render('pages/about');
 });
 
-app.get('/queue/enqueue', (req: Request, res: Response) => {
-    res.send('Hello, TypeScript Express!');
+app.post('/queue/enqueue', (req: Request, res: Response) => {
+    try {
+        queue.enqueue(req.body.value);
+        res.send(`Ein Element wurde hinzugefügt ${req.body.value}`);
+        res.send(`<a class="nav-link" href="/">zurück</a>`);
+      } catch (e) {
+        res.send(JSON.stringify(e));
+      }
+    
 });
 
 app.get('/queue/peek', (req: Request, res: Response) => {
-    res.send('Hello, TypeScript Express!');
+    queue.peek();
+    // res.send('Das erste Element ist ' + queue.peek());
+
+    var peek = queue.peek();
+
+    res.render('pages/peek', {
+        peek: peek,
+    });
 });
 
 app.get('/queue/dequeue', (req: Request, res: Response) => {
-    res.send('Hello, TypeScript Express!');
+    queue.dequeue();
+    var peek = queue.peek();
+
+    res.render('pages/peek', {
+        peek: peek,
+    });
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/queue/peek`);
+    console.log(`Server running at http://localhost:${port}`);
+});
+
+// index page
+app.get('/', function(req: Request, res: Response) {
+    var mascots = [
+        { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+        { name: 'Tux', organization: "Linux", birth_year: 1996},
+        { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+    ];
+    var tagline = "No programming concept is complete without a cute animal mascot.";
+
+    res.render('pages/index', {
+        mascots: mascots,
+        tagline: tagline
+    });
 });
